289e2339d5a67068244fc463527d9b65514d641b
Sargam <sargampoudel100@gmail.com>
2024-06-13 12:44:34 +0545

    fix(ui): minor changes

--- neet-result-frontend/src/layouts/home.tsx
+++ neet-result-frontend/src/layouts/home.tsx
@@ -3,13 +3,14 @@ import { ResultTable } from "../components/result-table";
 
 export function HomePage() {
   return (
-    <main className="flex flex-col gap-6 items-center py-6">
+    <main className="flex flex-col gap-6 items-center py-6 container">
       <h1 className="font-bold text-xl sm:text-2xl md:text-3xl">
         Neet Results <span className="underline">2024</span>
       </h1>
       <hr className="h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
       {/* Search Box */}
       <Search />
+      <hr className="h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
       {/* Result Table */}
       <ResultTable />
     </main>
